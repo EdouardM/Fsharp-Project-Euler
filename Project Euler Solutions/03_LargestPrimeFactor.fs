156d05165dcc611622f0205faefda35ce67c35d2
EdouardM <edouard.moureaux@gmail.com>
2015-05-21 13:42:50 +0200

    Covers case of 2 as prime + split of functions

--- Project Euler Solutions/03_LargestPrimeFactor.fs
+++ Project Euler Solutions/03_LargestPrimeFactor.fs
@@ -8,19 +8,23 @@ module Solution =
     ///Tells if a number is a Prime number
     let isPrime n = 
                 let rec testprime i =   if n % i = 0L then false
-                                        elif i >= n/2L then true
+                                        elif i > n/2L then true
                                         else testprime (i+1L)
-                testprime 2L
+                match n with
+                    | 2L -> true
+                    | _ ->  testprime 2L
     
-    ///Builds a sequence of prime factors and picks the max
+    ///Builds a sequence of prime factors
+    let primeFactors n = [1L.. n/2L]    |> Seq.filter (fun i -> n % i = 0L)
+                                        |> Seq.filter isPrime
+                                        
+    ///Picks the max prime factor or the number itself if its only factor is 1
     let largestPrimeFactor n = 
-                        let res =   [1L.. n/2L]  |> Seq.filter (fun i -> n % i = 0L)
-                                                 |> Seq.filter isPrime
-                                                 |> Seq.max
+                        let res =   primeFactors n |> Seq.max
                         if res = 1L then n else res
 
     ///////////// INPUT OUTPUT ////////////////////
-    //Read input in Console, cast to int64:
+    ///Reads input in Console, casts to int64
     let consoleReadInt() =  
                             let str  = Console.ReadLine()
                             try
