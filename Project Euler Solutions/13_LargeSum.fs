7db8c023f585341bbede1c21775a2d9173bfd58e
Edouard Moureaux <edouard.moureaux@gmail.com>
2015-11-25 22:28:59 +0100

    sync on new computer

---
+++ Project Euler Solutions/13_LargeSum.fs
@@ -0,0 +1,86 @@
+﻿namespace ProjectEuler.LargeSum
+
+module Solution =
+    open System
+    open Checked
+
+    ///Turns a number in an array of digits
+    let getDigits (str:string) =
+                       str  |> Array.ofSeq
+                            //convert char to string then int
+                            |> Array.map (string >> int)
+    
+    let sumArray arr =  
+                        let memo = ref 0
+                        let arr' = [| for d in arr do
+                                        let res = d + !memo
+                                        match res with
+                                            | r when r < 10 ->
+                                                    memo := 0
+                                                    yield r
+                                            | r' ->
+                                                    let res' = r' - 10
+                                                    memo := 1
+                                                    yield res' |]
+                        //If memo <> 0 then array is bigger:
+                        match !memo with
+                            | 0 -> arr'
+                            | m -> Array.concat [arr'; [|m|]]
+
+    let augment offset arr = 
+                        let prefix = Array.init offset (fun i -> 0)
+                        Array.concat [prefix ; arr]
+    
+    //To have arrays of same length
+    let arrangeLength arr1 arr2 = 
+                let l1 = arr1 |> Array.length
+                let l2 = arr2 |> Array.length
+                if l1 > l2 then
+                    arr1, augment (l1 - l2) arr2
+                elif l1 < l2 then 
+                    augment (l2 - l1) arr1, arr2
+                else
+                    arr1, arr2
+
+
+    let sum arr1 arr2 = 
+                        let arr1,arr2 = arrangeLength arr1 arr2
+                        Array.zip arr1 arr2
+                        |> Array.map (fun (i,j) -> i + j)
+                        // Sum in the good order
+                        |> Array.rev
+                        |> sumArray
+                        |> Array.rev
+      
+    ///////////// INPUT OUTPUT //////////////////// 
+    ///Reads input in Console, casts to int
+    let consoleReadInt() =  Console.ReadLine() |> int
+
+    let solution() = 
+         let n = consoleReadInt() 
+         let bigSum = Array.init n (fun i -> Console.ReadLine())
+                      |> Array.map getDigits
+                      //Sum big numbers:
+                      |> Array.reduce(fun arr1 arr2 -> sum arr1 arr2)
+        
+
+         //Pick only 10 first digits:
+         bigSum.[0..9] |> Array.map string
+                       |> Array.fold(fun d d' -> d + d') ""
+                       //print to console
+                       |> printfn "%s"
+
+//Build the program which runs in Console & HackerRank:
+module Program = 
+    open Solution
+    
+    //Main program:
+    //[<EntryPoint>]
+    let main argv = 
+        solution()
+        0
+
+
+
+
+                        
---
+++ Project Euler Solutions/17_NumberToWords.fs
@@ -0,0 +1,80 @@
+﻿namespace ProjectEuler.NumberToWords
+
+
+module Solution =
+    open System
+
+    let toWord n = 
+        try
+            match n with
+             | 1L -> "One" | 2L -> "Two" | 3L -> "Three" | 4L -> "Four"
+             | 5L -> "Five" | 6L -> "Six" | 7L -> "Seven" | 8L -> "Eight" | 9L -> "Nine"
+             | 10L -> "Ten" | 11L -> "Eleven" | 12L -> "Twelve" | 13L -> "Thirteen"
+             | 14L -> "Fourteen" | 15L -> "Fifteen" | 16L -> "Sixteen" | 17L -> "Seventeen"
+             | 18L -> "Eighteen" | 19L -> "Nineteen" | 20L -> "Twenty" | 30L -> "Thirty"
+             | 40L -> "Forty" | 50L -> "Fifty" | 60L -> "Sixty" | 70L -> "Seventy"
+             | 80L -> "Eighty" | 90L -> "Ninety"
+             | 100L -> "Hundred" | 1000L -> "Thousand"
+             | 1000000L -> "Million"
+             | 1000000000L -> "Billion"
+             | 1000000000000L -> "Trillion"
+        with
+            ex -> sprintf "Unexpected input: %d" n
+
+    
+    let (|Upto20|Tens|Hundreds|Thousands|Millions|Billions|Trillions|) =
+        function
+            | n when n >= 1L && n <= 20L -> Upto20 n
+            | n when n < 100L -> Tens n
+            | n when n >= 100L && n < 1000L -> Hundreds n
+            | n when n >= 1000L && n < 1000000L -> Thousands n
+            | n when n >= 1000000L && n < 1000000000L -> Millions n
+            | n when n >= 1000000000L && n < 1000000000000L -> Billions n
+            | n -> Trillions n
+
+    let rec convert n =
+        let helper boundary n =
+            let modulo = n % boundary
+            let units = n / boundary
+            if modulo > 0L then 
+                (convert units) + " " + (boundary |> toWord) + " " + (convert modulo)
+            else
+                (convert units) + " " + (boundary |> toWord)
+
+        match n with
+        | Upto20 n         -> toWord n
+        | Tens n
+            ->
+            let modulo = n % 10L
+            let remainder = modulo
+            let tens = (n - remainder) |> toWord
+            if remainder > 0L then 
+                tens + " " + (remainder |> toWord)
+            else tens
+        | Hundreds n    -> helper 100L n
+        | Thousands n   -> helper 1000L n
+        | Millions n    -> helper 1000000L n
+        | Billions n    -> helper 1000000000L n
+        | Trillions n   -> helper 1000000000000L n
+        
+
+    ///////////// INPUT OUTPUT //////////////////// 
+    ///Reads input in Console, casts to int64
+    let consoleReadInt() =  Console.ReadLine() |> int
+
+    let solution() = 
+         let n = consoleReadInt() 
+         Array.init n (fun i -> Console.ReadLine() |> int64)
+         |> Array.map (fun i -> if i = 0L then "Zero" else (convert i) )
+         |> Array.map (fun s -> printfn "%s" s)
+         |> ignore    
+
+//Build the program which runs in Console & HackerRank:
+module Program = 
+    open Solution
+    
+    //Main program:
+    //[<EntryPoint>]
+    let main argv = 
+        solution()
+        0
