9d033ce970ad38d6df6ae7217c33129e3aa3c295
Edouard Moureaux <edouard.moureaux@gmail.com>
2016-05-19 23:57:10 +0200

    Solution to MaximumPathSum

--- Project Euler Solutions/18_MaximumPathSum1.fs
+++ Project Euler Solutions/18_MaximumPathSum1.fs
@@ -3,24 +3,37 @@ namespace ProjectEuler.MaximumPathSum
 
 module Solution =
     open System
+    open System.Collections.Generic
     
     type Number = { Position: int; Value: int }
     
-    let rec loop numbers acc =
-        match numbers with 
-            | [] -> acc 
-            | l::ls -> 
-                let pos, sum = acc
-                let num = 
-                    l
-                    |> List.filter(fun n -> n.Position = pos || n.Position = pos + 1) 
-                    |> List.maxBy(fun n -> 
-                        let newAcc = n.Position, n.Value + sum
-                        loop ls newAcc |> snd)
-                let newAcc = num.Position, num.Value + sum
-                printfn "newAcc: pos:%d // sum: %d" num.Position (snd newAcc)
-                loop ls newAcc
+    let allPaths (numbers: Number list list) =
+        //List of rows = numbers list
+        numbers
+        |> List.fold(fun acc row ->
+                //State: all possible paths through rows
+                acc 
+                |> List.collect(fun path -> 
+                    let num = path |> List.head
+                    //For a new row
+                    row
+                    //Select valid positions
+                    |> List.filter(fun n -> n.Position = num.Position || n.Position = num.Position + 1 )
+                    //Add position to possible paths 
+                    |> List.map(fun n -> n::path))) 
+                    //Start state for fold
+                    [[ {Position = 0; Value = 0} ]]
+        //Reverse and drop start state
+        |> List.map (List.rev >> List.tail)
                 
+    let pathSum = 
+        List.map (fun num -> num.Value)
+        >> List.reduce (+)
+    
+    let solution = 
+        allPaths 
+        >> List.map pathSum
+        >> List.max
                 
     let readInput() =
         //Number of rows of triangle
@@ -38,16 +51,20 @@ module Solution =
         let t = Console.ReadLine() |> int
         Array.init t (fun _ ->
         let l = readInput()
-        loop l (0, 0)
-        |> snd
+        solution l
         |> printfn "%d")
         |> ignore
         0
     
-    (*
-    let l =
+    
+
+
+
+
+(*   let l =
         [ [3]; [7;4]; [2;4;6]; [8;5;9;3]]
         |> List.map (List.mapi(fun i x -> {Position = i; Value = x}))
     
-    loop l (0,0)
-    *)
+    solution  l 
+    
+*)
